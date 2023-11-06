6f88cda09447bf1986dba7b26287cc1418fe1150
Abhinavkumar Singh <abhinavrajput2011@gmail.com>
2023-11-06 13:12:33 -0500

    Time: 42 ms (50.55%), Space: 16.5 MB (40.82%) - LeetHub

--- 0541-reverse-string-ii/0541-reverse-string-ii.py
+++ 0541-reverse-string-ii/0541-reverse-string-ii.py
@@ -15,12 +15,32 @@ class Solution:
         
         above Pseudo-code iterates over the string s in chunks of 2k characters. For each chunk, it checks if the chunk has less than k characters. If so, it reverses all of the characters in the chunk. Otherwise, it reverses the first k characters in the chunk and leaves the others as original.
         """
-        s=list(s)
-        for i in range(0, len(s), 2 * k):
-            if i + k < len(s):
-                s[i:i + k] = s[i:i + k][::-1]
-            else:
-                s[i:] = s[i:][::-1]
+        # s=list(s)
+        # for i in range(0, len(s), 2 * k):
+        #     if i + k < len(s):
+        #         s[i:i + k] = s[i:i + k][::-1]
+        #     else:
+        #         s[i:] = s[i:][::-1]
         
+        # return "".join(s)
+
+
+        """
+        optimization on above
+
+        """
+        if k >= len(s):
+            return s[::-1]
+        
+        i = 0
+        s = list(s)
+        while i < len(s):
+            l = i
+            h = (i + k - 1) if (i + k - 1) < len(s) else len(s) - 1
+            while l < len(s) and l < h:
+                s[l], s[h] = s[h], s[l]
+                l += 1
+                h -= 1
+            i += (2 * k)
         return "".join(s)
 
