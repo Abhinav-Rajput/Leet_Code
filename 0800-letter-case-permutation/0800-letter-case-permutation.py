ede2230e1f856a53058f79a28c364450465463ad
Abhinavkumar Singh <abhinavrajput2011@gmail.com>
2023-11-07 18:51:51 -0500

    Time: 54 ms (84.68%), Space: 17.6 MB (41.05%) - LeetHub

--- 0800-letter-case-permutation/0800-letter-case-permutation.py
+++ 0800-letter-case-permutation/0800-letter-case-permutation.py
@@ -1,19 +1,46 @@
 class Solution:
     def letterCasePermutation(self, s: str) -> List[str]:
 
-        result =[]
+        # result =[]
 
-        def helper(s,i, slate ):
-            if i == len(s):
-                result.append(slate)
+        # def helper(s,i, slate ):
+        #     if i == len(s):
+        #         result.append(slate)
+        #     else:
+        #         if s[i].isdigit():
+        #             helper(s,i+1,slate+s[i])
+        #         else:
+        #             helper(s,i+1,slate+s[i].upper())
+        #             helper(s,i+1,slate+s[i].lower())
+        
+        # helper(s,0,"")
+
+        # return result
+
+        # above solution is  with immutable parameter
+
+        # following solution with mutable parameter
+
+        result=[]
+
+        def mutableHelper(s,i,slate):
+            if i== len(s):
+                result.append("".join(slate))
+                return
             else:
                 if s[i].isdigit():
-                    helper(s,i+1,slate+s[i])
+                    slate.append(s[i])
+                    mutableHelper(s,i+1, slate)
+                    slate.pop()
                 else:
-                    helper(s,i+1,slate+s[i].upper())
-                    helper(s,i+1,slate+s[i].lower())
-        
-        helper(s,0,"")
+                    slate.append(s[i].lower())
+                    mutableHelper(s,i+1,slate)
+                    slate.pop()
+
+                    slate.append(s[i].upper())
+                    mutableHelper(s,i+1,slate)
+                    slate.pop()
+
+        mutableHelper(s,0,[])
 
         return result
-        
